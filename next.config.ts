54ef19b97a3e393bd8c2f287c56f5face6b5e60c
bettaibi <bettaibinidhal00@gmail.com>
2025-10-30 23:19:15 +0100

    added cloudflare opennext integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+initOpenNextCloudflareForDev();
+
 const nextConfig: NextConfig = {
+  experimental: {
+    typedRoutes: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   /* config options here */
+  serverExternalPackages: ["@libsql/isomorphic-ws", "@libsql/client"],
 };
 
 export default nextConfig;
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,8 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client/web";
+import { drizzle } from "drizzle-orm/libsql/web";
 
-const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+export const db = drizzle({
+  connection: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+  },
 });
-
-export const db = drizzle(turso);
